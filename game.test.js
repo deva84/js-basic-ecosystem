1c979962b856b6d067ac4cfa4bac6f30deb09316
Svitlana Dasgupta <svitlana_dasgupta@epam.com>
2021-10-24 20:52:24 +0300

    test: refactor test case to check constructor

--- game.test.js
+++ game.test.js
@@ -1,8 +1,8 @@
 import { Game } from './main'
 describe('Darts 301', () => {
-  it('should handle clean game', () => {
-    const game = new Game();
-    expect(game).toBeTruthy();
+  it('should create a game with a provided number of players', () => {
+    const game = new Game(2);
+    expect(game.players).toBe(2);
   })
 
 });
