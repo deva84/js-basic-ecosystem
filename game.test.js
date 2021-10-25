50fc6484ea276aeca224cb24aecb015106ef7ebf
Svitlana Dasgupta <svitlana_dasgupta@epam.com>
2021-10-26 00:41:10 +0300

    test: add test cases for completed game scenario

--- game.test.js
+++ game.test.js
@@ -30,4 +30,29 @@ describe('Darts 301', () => {
     expect(game.score(0)).toEqual(238);
     expect(game.score(1)).toEqual(216);
   });
+
+  describe('Completed game scenario', () => {
+    let completedGame;
+
+    beforeEach(() => {
+     completedGame = () => game
+        .throw(8, 2, 0).throw(2, 1, 0).throw(15, 3, 0).throw(1, 3, 1).throw(20, 2, 1).throw(14, 3, 1)
+        .throw(9, 3, 0).throw(14, 1, 0).throw(2, 2, 0).throw(4, 2, 1).throw(19, 1, 1).throw(7, 3, 1)
+        .throw(11, 1, 0).throw(3, 3, 0).throw(20, 2, 0).throw(16, 3, 1).throw(12, 2, 1).throw(13, 1, 1)
+        .throw(15, 1, 0).throw(20, 3, 0).throw(7, 1, 0).throw(1, 1, 1).throw(18, 1, 1).throw(10, 1, 1)
+        .throw(6, 2, 0).throw(1, 1, 0).throw(4, 1, 0).throw(19, 1, 1).throw(17, 1, 1).throw(18, 1, 1);
+    });
+
+    it('should throw exception when one of players scored 0', () => {
+      expect(completedGame().players[1].score).toEqual(0);
+      expect(completedGame().throw(7, 1, 0)).toThrow();
+    });
+
+    it ('should return a winner index when a winner is decided', () => {
+      expect(completedGame().players[1].score).toEqual(0);
+      expect(completedGame().winner).toEqual(0);
+    })
+  })
+
+
 });
